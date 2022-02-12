dacec164acab3382f01782c7397669c8edc0ec88
tadashim <ma.tadashi@gmail.com>
2022-02-13 00:48:44 +0900

    change root response

--- packages/backend/src/app.controller.spec.ts
+++ packages/backend/src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello, this is 和組 community bot"', () => {
+      expect(appController.getHello()).toBe('Hello, this is 和組 community bot');
     });
   });
 });
