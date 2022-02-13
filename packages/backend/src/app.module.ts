6c7295c7ab1e7a684aa56748809f7ef548fb9a84
tadashim <ma.tadashi@gmail.com>
2022-02-13 12:15:07 +0900

    add ping controller and service

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PingController } from './ping/ping.controller';
+import { PingService } from './ping/ping.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, PingController],
+  providers: [AppService, PingService],
 })
 export class AppModule {}
