b088ca29955a396ea663268dc6872a91ca9beda5
tadashim <ma.tadashi@gmail.com>
2022-02-13 15:45:30 +0900

    fix: eslintrc

--- packages/dev-team-bot/.eslintrc.js
+++ packages/dev-team-bot/.eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     project: 'tsconfig.json',
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'prettier'],
   extends: [
     'plugin:@typescript-eslint/recommended',
   ],
@@ -19,5 +19,6 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': 'error',
   },
 };
