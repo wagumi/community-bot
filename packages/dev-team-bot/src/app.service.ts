ca72ddfbdb43581acd3717da418dc5ee3f87ad07
kennyt <6182830+kennyttotheleft@users.noreply.github.com>
2022-02-14 00:15:03 +0900

    chore: update hello message

--- packages/dev-team-bot/src/app.service.ts
+++ packages/dev-team-bot/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello, This is 和組 community bot';
+    return 'Hello, This is 和組 community bot.';
   }
 }
